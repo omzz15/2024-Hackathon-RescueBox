30001ad4f3fd69c391978ce0095044d8bb10a10a
om <ompatel1359@gmail.com>
2024-10-25 13:33:17 -0400

    trying to isolate background

--- tool-suite/cluster-settings/model.py
+++ tool-suite/cluster-settings/model.py
@@ -1,23 +1,26 @@
-from pathlib import Path
-from typing import Optional
-from cv2 import Mat
-import numpy as np
+import glob
+from PIL import Image
+from torchvision import transforms
+import torch
+import numpy
+import cv2
 
 class ClusterSettingModel:
     def __init__(self):
-        import inference
-        self.model = inference.get_model(model_id="yolov8n-seg-640")
+        import torch
+        self.model = torch.hub.load('pytorch/vision:v0.10.0', 'deeplabv3_resnet50', pretrained=True)
+        self.model.eval()
 
-    def get_image_files(self, directory: str) -> dict[Path, Mat]:
-        
+    def get_image_files(self, directory: str) -> list[str]:
         # TODO maybe do this without loading all images?
         print(f"Loading images from {directory}...")
         files = glob.glob(directory + "/**/*")
-        images = {}
+        images = []
         for file in files:
             try:
-                img = cv2.imread(file)
-                images[Path(file)] = img
+                with Image.open(file) as f:
+                    f.verify()
+                images.append(file)
             except:
                 print(f"Unable to open {file} as image")
 
@@ -25,5 +28,33 @@ class ClusterSettingModel:
 
         return images
 
-    def remove_human(self, img : Mat) -> Mat:
-        result = self.model.infer(img)[0]
+    def remove_human(self, img_path: str) -> None:
+        input_image = Image.open(img_path)
+        input_image = input_image.convert("RGB")
+
+        preprocess = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
+
+        input_tensor = preprocess(input_image)
+        input_batch = input_tensor.unsqueeze(0)
+
+        with torch.no_grad():
+            output = self.model(input_batch)['out'][0]
+        output_predictions = output.argmax(0)
+
+        # # create a color pallette, selecting a color for each class
+        palette = torch.tensor([2 ** 25 - 1, 2 ** 15 - 1, 2 ** 21 - 1])
+        colors = torch.as_tensor([i for i in range(21)])[:, None] * palette
+        colors = (colors % 255).numpy().astype("uint8")
+
+        # # plot the semantic segmentation predictions of 21 classes in each color
+        r = Image.fromarray(output_predictions.byte().cpu().numpy()).resize(input_image.size)
+        r.putpalette(colors)
+
+        open_cv_image = numpy.array(r)
+        cv2.imshow("img", open_cv_image)
+        cv2.imshow("img2", open_cv_image)
+
+
